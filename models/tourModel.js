a496a5c79364e5f3503483eb1dc2c18a7f7a7cee
Kenneth Man <kenneth.man@ntegra.co.uk>
2022-11-02 22:20:04 +0000

    after 107

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,58 +1,131 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, 'A tour must have a name'],
-		unique: true,
-		trim: true
+const tourSchema = new mongoose.Schema(
+	// schema definitons
+	{
+		name: {
+			type: String,
+			required: [true, 'A tour must have a name'],
+			unique: true,
+			trim: true
+		},
+		slug: String,
+		duration: {
+			type: Number,
+			required: [true, 'A tour must have a duration']
+		},
+		maxGroupSize: {
+			type: Number,
+			required: [true, 'A tour must have a group size']
+		},
+		difficulty: {
+			type: String,
+			required: [true, 'A tour must have a difficulty']
+		},
+		ratingsAverage: {
+			type: Number,
+			default: 4.5
+		},
+		ratingsQuantity: {
+			type: Number,
+			default: 0
+		},
+		price: {
+			type: Number,
+			required: [true, 'A tour must have a price']
+		},
+		priceDiscount: Number,
+		summary: {
+			type: String,
+			trim: true,
+			required: [true, 'A tour must have a summary']
+		},
+		description: {
+			type: String,
+			trim: true
+		},
+		imageCover: {
+			type: String,
+			required: [true, 'A tour must have a cover image']
+		},
+		// of type 'string[]'
+		images: [String],
+		createdAt: {
+			type: Date,
+			default: Date.now(),
+			select: false
+		},
+		startDates: [Date],
+		secretTour: {
+			type: Boolean,
+			default: false
+		}
 	},
-	duration: {
-		type: Number,
-		required: [true, 'A tour must have a duration']
-	},
-	maxGroupSize: {
-		type: Number,
-		required: [true, 'A tour must have a group size']
-	},
-	difficulty: {
-		type: String,
-		required: [true, 'A tour must have a difficulty']
-	},
-	ratingsAverage: {
-		type: Number,
-		default: 4.5
-	},
-	ratingsQuantity: {
-		type: Number,
-		default: 0
-	},
-	price: {
-		type: Number,
-		required: [true, 'A tour must have a price']
-	},
-	priceDiscount: Number,
-	summary: {
-		type: String,
-		trim: true,
-		required: [true, 'A tour must have a summary']
-	},
-	description: {
-		type: String,
-		trim: true
-	},
-	imageCover: {
-		type: String,
-		required: [true, 'A tour must have a cover image']
-	},
-	// of type 'string[]'
-	images: [String],
-	createdAt: {
-		type: Date,
-		default: Date.now(),
-		select: false
-	},
-	startDates: [Date]
+	//schema options
+	{
+		toJSON: {
+			virtuals: true
+		},
+		toObject: {
+			virtuals: true
+		},
+	});
+
+// virtual property; used to perform operation on a schema property, then assign a new property
+// cannot be used in queries
+tourSchema.virtual('durationWeeks').get(function() {
+	// using a function declaration so we have access to 'this' keyword which refers to the current document
+	return this.duration / 7;
+});
+
+// 4 types of mongoose middleware: DOCUMENT, QUERY, AGGREGATE, MODEL
+//////////////////////////////////////////////////////////////////////
+// --- DOCUMENT middleware; runs before or after the currently processed document is saved
+// 'save' hook only runs for the '.save()' or '.create()' mongoose methods
+// every middleware function has access to 'next'
+tourSchema.pre('save', function(next) {
+	this.slug = slugify(this.name, { lower: true });
+	next();
+});
+
+tourSchema.pre('save', function(next) {
+	console.log('pre save hook to do something');
+	next();
+});
+
+// executed once all pre() middleware functions have completed
+// acts on the saved document
+tourSchema.post('save', function(document, next) {
+	console.log(document);
+	next();
+});
+
+// --- QUERY middleware; runs before or after a query is executed
+// any hook that begins with 'find' will run this middleware function (e.g. find, findOne, findOneAndDelete...)
+tourSchema.pre(/^find/, function(next) {
+	// find all documents where 'secretTour' is not equal to true
+	// 'this' refers to the current query
+	this.find({ secretTour: { $ne: true } });
+	this.start = Date.now();
+	next();
+});
+
+tourSchema.post(/^find/, function(documents, next) {
+	console.log(`Query took ${Date.now() - this.start}ms`);
+	console.log(documents)
+	next();
+});
+
+// --- AGGREGATION MIDDLEWARE; runs before or after an aggregation is executed
+tourSchema.pre('aggregate', function(next) {
+	// 'this' refers to the current aggregation object
+	this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+	next();
+});
+
+tourSchema.post('aggregate', function(next) {
+	next();
 });
 
 // creating a model from a schema
